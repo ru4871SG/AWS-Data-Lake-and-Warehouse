a5dc550a5e92a898fcc511cdc9355aa0c70a21b5
Ruddy Setiadi Gunawan <ntcv_mr68@outlook.com>
2024-10-13 22:19:02 -0500

    Upload sql statement for redshift

---
+++ redshift_create_table.sql
@@ -0,0 +1,19 @@
+-- This table should be created in Redshift before executing AWS Glue ETL job to transfer data from S3 to Redshift
+
+CREATE TABLE amazonbestsellers_redshift (
+    unique_id BIGINT IDENTITY(1,1),
+    product_num_ratings INT,
+    rank INT,
+    product_star_rating DOUBLE PRECISION,
+    category VARCHAR(255),
+    product_photo VARCHAR(1000),
+    product_url VARCHAR(1000),
+    currency VARCHAR(10),
+    short_product_title VARCHAR(500),
+    product_title VARCHAR(1000),
+    asin VARCHAR(50) NOT NULL,
+    fetch_timestamp TIMESTAMP,
+    product_price NUMERIC(10,2)
+);
+
+COMMIT;
